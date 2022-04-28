acc17b8ea49d57fc165ba1b99a873aaf0cffecba
HP <HP@192.168.0.5>
2022-04-28 12:14:48 +0530

    NewProject

---
+++ TestngGitJen/src/test/java/com/TestScripts/TestClass.java
@@ -0,0 +1,27 @@
+package com.TestScripts;
+
+import org.testng.annotations.Test;
+
+import com.base.BaseClass;
+
+public class TestClass extends BaseClass {
+	
+	
+	@Test
+	public void testCaseone()
+	{
+		System.out.println("This is test case 1");
+	}
+	@Test
+	public void testCasetwo()
+	{
+		System.out.println("This is test case 2");
+	}
+	@Test
+	public void testCasethree()
+	{
+		System.out.println("This is test case 3");
+	}
+	
+	
+}
